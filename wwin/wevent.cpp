4892e5e1aa28830cb3beabd5920b2a2f26b910c1
d3i0 <d3i0@ya.ru>
2017-04-21 19:44:40 +0600

    WEvent documentation ready

--- wwin/wevent.cpp
+++ wwin/wevent.cpp
@@ -1,31 +1,51 @@
 #include "wevent.h"
 
+/**
+ * @brief WEvent::WEvent Конструктор класса события
+ * @param type тип события
+ */
 WEvent::WEvent(WEvent::Type type)
     : _type(type)
-{
+{}
 
-}
-
+/**
+ * @brief WEvent::accept пометить что событие обработано.
+ */
 void WEvent::accept()
 {
     _accepted = true;
 }
 
+/**
+ * @brief WEvent::ignore пометить что событие проигнорировано.
+ */
 void WEvent::ignore()
 {
     _accepted = false;
 }
 
+/**
+ * @brief WEvent::isAccepted проверить обработано ли событие
+ * @return
+ */
 bool WEvent::isAccepted() const
 {
     return _accepted;
 }
 
+/**
+ * @brief WEvent::setAccepted установить обработано событие или нет
+ * @param accepted
+ */
 void WEvent::setAccepted(bool accepted)
 {
     _accepted = accepted;
 }
 
+/**
+ * @brief WEvent::type Получить тип события
+ * @return
+ */
 WEvent::Type WEvent::type()
 {
     return _type;
