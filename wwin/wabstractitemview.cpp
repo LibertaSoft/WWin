f0b29e8288a981a45e35451695a5c32dbb8451d8
d3i0 <d3i0@ya.ru>
2017-04-21 23:44:25 +0600

    WAbstractItemView documentation ready

--- wwin/wabstractitemview.cpp
+++ wwin/wabstractitemview.cpp
@@ -1,22 +1,40 @@
 #include "wabstractitemview.h"
 
+/**
+ * @brief WAbstractItemView::currentIndex получить индекс текущего элемента
+ * @return
+ */
 WModelIndex WAbstractItemView::currentIndex() const
 {
     return _selectedIndex;
 }
 
+/**
+ * @brief WAbstractItemView::WAbstractItemView Конструктор с инициализацией базового класса
+ * @param parent
+ */
 WAbstractItemView::WAbstractItemView(WWidget *parent)
     : WWidget(parent)
-{
+{}
 
-}
-
+/**
+ * @brief WAbstractItemView::setModel Метод для утановки модели.
+ * @warning Обязательно должен быть вызван при переопределении.
+ * @param model
+ */
 void WAbstractItemView::setModel(WAbstractItemModel *model)
 {
     _abstractModel = model;
     model->__addUpdateListener( this );
 }
 
+/**
+ * @brief WAbstractItemView::dataChanhed Метод получающий информацию о изменениях в модели
+ * и инициализирующий обновление данных во View
+ * @param topLeft - Индекс начала изменившегося фрагмента
+ * @param bottomRight - Индекс конца изменившегося фрагмента
+ * @param roles - [Не используется] роли
+ */
 void WAbstractItemView::dataChanhed(const WModelIndex topLeft, const WModelIndex bottomRight, const std::vector<int> roles)
 {
     for (int row = topLeft.row; row < bottomRight.row; ++row) {
--- wwin/wabstractitemview.h
+++ wwin/wabstractitemview.h
@@ -4,17 +4,25 @@
 #include "wwin/wwidget.h"
 #include "wwin/wabstractitemmodel.h"
 
+/**
+ * @brief WAbstractItemView Базовый класс для всех View
+ *
+ * Является частью системы Model-View реализованной в WWin.
+ *
+ * Для реализации своего View необходимо унаследовать этот класс и реализовать недостающие методы.
+ * При реализации метода setModel у вашего класса, необходимо обязательно вызвать метод базового
+ * класса: `WAbstractItemView::setModel(model);` В нём реализуется важная часть системы Model-View.
+ */
 class WAbstractItemView : public WWidget
 {
 private:
-    WAbstractItemModel *_abstractModel;
+    WAbstractItemModel *_abstractModel; /// < [Не используется] Указатель на модель
 protected:
-    WModelIndex _selectedIndex = {0,0};
+    WModelIndex _selectedIndex = {0,0}; /// < Индекс выделенного элемента \todo сделать группу выделения
 
-private:
-    WModelIndex currentIndex() const;
 public:
     WAbstractItemView(WWidget *parent);
+    WModelIndex currentIndex() const;
 
     virtual void setModel(WAbstractItemModel *model);
 
